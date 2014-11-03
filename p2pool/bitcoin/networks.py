04bc3a1d9e88a01ae086701bf727ba06cd6d92fb
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-11-03 11:22:54 -0500

    Minor fix to enable proper launching
    Changed skein_hash to skeinhash. Fixed launch issue related to that

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -350,7 +350,7 @@ nets = dict(
             not (yield bitcoind.rpc_getinfo())['testnet']
         )),
         SUBSIDY_FUNC=lambda height: 1000*100000000 >> (height + 1)//967680,
-	POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('skein_hash').getPoWHash(data)),
+	POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('skeinhash').getPoWHash(data)),
         BLOCK_PERIOD=30, # s targetspacing
         SYMBOL='MYR',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'myriadcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/myriadcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.myriadcoin'), 'myriadcoin.conf'),
